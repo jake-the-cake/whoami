005a09534d4d44d4e4986ee1e4e4eb55a52e74de
jake-the-cake <askjake331@gmail.com>
2025-01-20 15:09:10 -0500

    hide button

--- app/static/buttons.js
+++ app/static/buttons.js
@@ -1,1 +1,6 @@
-console.log('hi')
+document.addEventListener('DOMContentLoaded', () => {
+    navbuttons = Array.from(document.querySelector('nav').children)
+    navbuttons.forEach(button => {
+        if (button.href === window.location.href) button.style.display = 'none'
+    });
+})
