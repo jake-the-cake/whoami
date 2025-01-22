bc6bd748af8e35b919a403f997b268b635d7883c
jake-the-cake <askjake331@gmail.com>
2025-01-22 10:09:45 -0500

    refactor

--- app/static/buttons.js
+++ app/static/buttons.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    removeCurrentPageLink()
+})
+
+function removeCurrentPageLink(href = window.location.href) {
     navbuttons = Array.from(document.querySelector('nav').children)
     navbuttons.forEach(button => {
-        if (button.href === window.location.href) button.style.display = 'none'
+        if (button.href === href) button.style.display = 'none'
     });
-})
+}
