7ac9e47cd329534b8aa0ec5ee458d249ca441c3d
jake-the-cake <askjake331@gmail.com>
2025-01-23 09:16:11 -0500

    select form

--- app/static/change-form.js
+++ app/static/change-form.js
@@ -4,12 +4,22 @@ register = document.querySelector('#registration-form')
 login_link = register.querySelector('span')
 register_link = login.querySelector('span')
 
-const swapShrink = () => {
-    Array(register, login).forEach(cls => {
+buttons = Array.from(login.parentNode.querySelectorAll('button'))
+login_btn = buttons.filter(btn => btn.innerText == 'Login')[0]
+register_btn = buttons.filter(btn => btn.innerText == 'Register')[0]
+button_container = login_btn.parentNode
+console.log(button_container)
+
+swapShrink([login, register])
+
+register.classList.add('shrinkY')
+login_btn.addEventListener('click', () => swapShrink([login, button_container]))
+register_btn.addEventListener('click', () => swapShrink([register, button_container]))
+login_link.addEventListener('click', () => swapShrink([login, register]))
+register_link.addEventListener('click', () => swapShrink([login, register]))
+
+function swapShrink(array) {
+    array.forEach(cls => {
         cls.classList.toggle('shrinkY')
     })
 }
-
-register.classList.add('shrinkY')
-login_link.addEventListener('click', swapShrink)
-register_link.addEventListener('click', swapShrink)
